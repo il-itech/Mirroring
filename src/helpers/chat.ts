b73ab1a196336d94f76c17fbfea78809d19e8cd3
yablonsky.il <yablonsky.il@yandex.by>
2020-10-03 18:16:15 +0300

    refactor: update ts

--- src/helpers/chat.ts
+++ src/helpers/chat.ts
@@ -7,10 +7,10 @@ export const getLastMessage = (
   _id: string,
   messages: IChatMessages,
 ): string =>
-  R.compose<IChatMessage[], IChatMessage, string>(
+  R.compose<IChatMessages, IChatMessage[], IChatMessage, string>(
     R.propOr('...', 'message'),
     R.last,
-    R.propOr([], _id),
+    R.propOr<[]>([], _id),
   )(messages);
 
 export const getRoomHash = (
